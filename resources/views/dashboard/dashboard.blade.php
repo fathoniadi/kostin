5376cda82c03f4a7e87bd97f1a0a1c70b0693c2b
Humaira Tri Acantya <contact.acantya@gmail.com>
2017-05-18 11:56:55 +0700

    edit tampilan

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -33,6 +33,7 @@
 							<div class="row">
 								<div class="col-sm-12 panel-kost" style="padding: 10px;">
 									<a href="{{ url('/dashboard/kamar/create') }}" class="btn btn-primary pull-right" title="" style="font-size:20px;">Tambah Kamar Kost</a>
+									<br>
 								</div>
 								@if ($kamars->count()>0)
 									@foreach ($kamars as $key => $kamar)
