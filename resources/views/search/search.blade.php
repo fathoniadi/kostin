6587e562169025ecfce9d5158a7c586c7dee6137
Fathoni Adi Kurniawan <fathoni.adi@gmail.com>
2017-05-18 11:54:38 +0700

    perbaiki tulisan harga

--- resources/views/search/search.blade.php
+++ resources/views/search/search.blade.php
@@ -220,23 +220,23 @@
 	                </div>
 	                <div class="col-md-4">
 	                    <div class="col-md-12">
-	                        <h3 @if ($kamar->jumlah_kamar==0)
+                            <h4 @if ($kamar->jumlah_kamar==0)
                             	style="color: orange" 
-                            @endif>Sisa : {{$kamar->jumlah_kamar}}</h3>
+                            @endif>Sisa : {{$kamar->jumlah_kamar}}</h4>
                             @if ($kamar->jumlah_kamar==0)
                             	<div style="padding: 10px;" class="btn-warning btn-fill text-center">
-                                <p>Kamar Tidak Tersedia</p>
+                                <h4>Kamar Tidak Tersedia</h4>
                             </div>
                             @else
-								<div style="padding: 10px;" class="btn-success btn-fill text-center">
-                                <h4>Rp. {{$kamar->harga_kamar}}/@if($kamar->sewa_kamar==1)
+								<div style="padding: 5px;" class="btn-success btn-fill text-center">
+                                <h3>Rp. {{$kamar->harga_kamar}}/@if($kamar->sewa_kamar==1)
                                 	Bulan
                                 @elseif($kamar->sewa_kamar==2)
                                 	Tahun
-                                @endif</h4>
+                                @endif</h3>
                             </div>
                             @endif
-	                    </div>
+                        </div>
 	                </div>
 	            	<div class="clearfix"></div>
 				</div>
